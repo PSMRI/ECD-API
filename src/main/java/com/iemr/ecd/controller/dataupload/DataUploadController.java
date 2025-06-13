616c49950470a1b267bafd0da91e5b61e861bd4c
vishwab1 <vishwanath@navadhiti.com>
2025-06-13 17:04:26 +0530

    implemented cors all api

--- src/main/java/com/iemr/ecd/controller/dataupload/DataUploadController.java
+++ src/main/java/com/iemr/ecd/controller/dataupload/DataUploadController.java
@@ -25,7 +25,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
+
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
@@ -41,7 +41,6 @@ import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 
 @RestController
-@CrossOrigin()
 public class DataUploadController {
 
 	@Autowired
--- src/main/java/com/iemr/ecd/controller/outboundworklist/CallStatisticsController.java
+++ src/main/java/com/iemr/ecd/controller/outboundworklist/CallStatisticsController.java
@@ -25,7 +25,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -42,7 +42,6 @@ import io.swagger.v3.oas.annotations.responses.ApiResponses;
 
 @RestController
 @RequestMapping(value = "/agent", headers = "Authorization")
-@CrossOrigin()
 public class CallStatisticsController {
 
 	@Autowired
