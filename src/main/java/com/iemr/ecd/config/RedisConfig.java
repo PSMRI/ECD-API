278d65460c9ef3ed4ac24eeb0623da6747c74bb9
DurgaPrasad-54 <prasad8790237@gmail.com>
2026-02-03 13:04:39 +0530

    chore(swagger): automate swagger sync to amrit-docs

--- src/main/java/com/iemr/ecd/config/RedisConfig.java
+++ src/main/java/com/iemr/ecd/config/RedisConfig.java
@@ -25,6 +25,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
@@ -33,6 +34,7 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 import com.iemr.ecd.dao.Users;
 
+@Profile("!swagger")
 @Configuration
 @EnableCaching
 public class RedisConfig {
